fe952ef29f1de25ef05dea7addf8d3558b8b8872
guitoji <guilherme.limaal22@outlook.com>
2026-03-30 21:37:22 -0300

    Subtotal & Total methods

--- course/src/main/java/com/educandoweb/course/entities/Order.java
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -93,6 +93,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        double sum = 0;
+        for (OrderItem obj : items) {
+            sum += obj.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
